6963eea20db9b258595e7b35c187d5f42605bdcd
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-11 16:00:21 -0300

    Import Sequelize to db connection

---
+++ auth-api/src/config/db/dbConfig.js
@@ -0,0 +1,21 @@
+import {Sequelize} from "sequelize";
+
+const sequelize = new Sequelize("auth-db", "admin", "123456", {
+    host: "localhost",
+    dialect: "postgres",
+    quoteIdentifiers: false,
+    define: {
+        syncOnAssociation: true,
+        timestamps: false,
+        underscored: true,
+        underscoredAll: true,
+        freezeTableName: true
+    }
+})
+
+sequelize.authenticate().then(() => {
+        console.info('Connection has ben established!')
+    }).catch(err => {
+        console.error("Unable to connect to the database!")
+        console.error(err.message)
+})

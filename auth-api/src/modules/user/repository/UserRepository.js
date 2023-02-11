156cb06704a48e2c3e30f6d61eb9d25a258ab334
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-11 20:28:26 -0300

    UserRepository

---
+++ auth-api/src/modules/user/repository/UserRepository.js
@@ -0,0 +1,22 @@
+import User from "../model/User.js";
+
+class UserRepository {
+
+    async findById(id) {
+        try {
+            return await User.findOne({ where: { id }})
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    async findByEmail(email) {
+        try {
+            return await User.findOne({ where: { email }})
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+}
+
+export default new UserRepository();

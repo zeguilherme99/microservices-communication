7e669d409c883aaf99df58e14e735bf2511cfe8b
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-11 20:29:02 -0300

    UserService

---
+++ auth-api/src/modules/user/service/UserService.js
@@ -0,0 +1,38 @@
+import UserRepository from "../repository/UserRepository.js";
+import * as httpStatus from "../../../config/constants/httpStatus"
+
+class UserService {
+
+    async findByEmail(req) {
+        try {
+            const { email } = req.params;
+            this.validateRequestData(email)
+            let user = UserRepository.findByEmail(email)
+            if (!user) {
+
+            }
+            return {
+                status: httpStatus.SUCCESS,
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                }
+            }
+        } catch (err) {
+            return {
+                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: err.status
+            }
+        }
+
+    }
+
+    validateRequestData(email) {
+        if (!email) {
+            throw new Error('User email was not informed.')
+        }
+    }
+}
+
+export default new UserService();

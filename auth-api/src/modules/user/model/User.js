cb4571f4131d55b422079de6a3b44749f286c935
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-11 20:04:44 -0300

    Db init

---
+++ auth-api/src/modules/user/model/User.js
@@ -0,0 +1,27 @@
+import {DataTypes} from "sequelize";
+
+import {sequelize} from "../../../config/db/dbConfig.js"
+
+const User = sequelize.define("user", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+    },
+    {}
+);
+
+export default User;
